89a369a7a94196a210c962c86bf75ebf4b687fa7
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-07 21:48:20 +0530

    import java.util.*; class vamsi { 	public static void main(String[] args) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int c=0; 		int arr[] = new int[n]; 		for(int i=0;i<n;i++) 		{ 		    arr[i]=sc.nextInt(); 		} 		int k = sc.nextInt(); 		for(int i=0;i<n;i++) 		{ 		    if(arr[i]==k) 		    { 		        c+=1; 		    } 		} 		System.out.print(c); 	} }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class vamsi
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int c=0;
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		int k = sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    if(arr[i]==k)
+		    {
+		        c+=1;
+		    }
+		}
+		System.out.print(c);
+	}
+}
