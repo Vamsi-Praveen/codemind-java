2759ccbedc2c0ac83342b564b21a49df2027b48e
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-07 21:30:29 +0530

    import java.util.*; class vamsi { 	public static void main(String[] args) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int c=0; 		int arr[] = new int[n]; 		for(int i=0;i<n;i++) 		{ 		    arr[i]=sc.nextInt(); 		} 		for(int i=0;i<n;i++) 		{ 		   if(arr[i]%2!=0) 		   { 		       System.out.printf("%d ",arr[i]); 		   } 		} 		for(int i=0;i<n;i++) 		{ 		   if(arr[i]%2==0) 		   { 		       System.out.printf("%d ",arr[i]); 		   } 		}
    	}
    }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class vamsi
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int c=0;
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+		   if(arr[i]%2!=0)
+		   {
+		       System.out.printf("%d ",arr[i]);
+		   }
+		}
+		for(int i=0;i<n;i++)
+		{
+		   if(arr[i]%2==0)
+		   {
+		       System.out.printf("%d ",arr[i]);
+		   }
+		}
+		
+	}
+}
