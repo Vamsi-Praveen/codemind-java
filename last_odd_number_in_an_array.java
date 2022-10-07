74946c2a34700913924551acb4cb1a77f56f574b
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-07 21:44:59 +0530

    import java.util.*; class vamsi { 	public static void main(String[] args) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int c=0; 		int arr[] = new int[n]; 		for(int i=0;i<n;i++) 		{ 		    arr[i]=sc.nextInt(); 		} 		for(int i=n-1;i>=0;i--) 		{ 		   if(arr[i]%2!=0) 		   { 		       System.out.printf("%d ",arr[i]); 		       break; 		   } 		}
    	}
    }

---
+++ last_odd_number_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class vamsi
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int c=0;
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=n-1;i>=0;i--)
+		{
+		   if(arr[i]%2!=0)
+		   {
+		       System.out.printf("%d ",arr[i]);
+		       break;
+		   }
+		}
+		
+	}
+}
