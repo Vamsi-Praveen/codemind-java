252786963fab3c25575cff320f765dc48bc14c5e
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-07 22:11:45 +0530

    import java.util.*; class vamsi { 	public static void main(String[] args) 	{ 		Scanner sc = new Scanner(System.in); 		float f= sc.nextFloat(); 		double c=((f-32)*5)/9; 		System.out.printf("%.2f",c); 	} }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class vamsi
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		float f= sc.nextFloat();
+		double c=((f-32)*5)/9;
+		System.out.printf("%.2f",c);
+	}
+}
