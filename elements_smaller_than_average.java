8594db553208654bc4dbd9e4789dc67249772c67
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-07 21:23:59 +0530

    import java.util.*; class vamsi { 	public static void main(String[] args) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int s=0; 		int c=0; 		int arr[] = new int[n]; 		for(int i=0;i<n;i++) 		{ 		    arr[i]=sc.nextInt(); 		} 		for(int i=0;i<n;i++) 		{ 		    s+=arr[i]; 		} 		int avg=s/n; 		for(int i=0;i<n;i++) 		{ 		    if(arr[i]<=avg) 		    { 		        c+=1; 		    } 		} 		System.out.print(c); 	} }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class vamsi
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int s=0;
+		int c=0;
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+		    s+=arr[i];
+		}
+		int avg=s/n;
+		for(int i=0;i<n;i++)
+		{
+		    if(arr[i]<=avg)
+		    {
+		        c+=1;
+		    }
+		}
+		System.out.print(c);
+	}
+}
