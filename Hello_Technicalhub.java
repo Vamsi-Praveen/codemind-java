efc5a85e88c501a3e9fdd11088ebc9538baf9e48
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-08 16:15:27 +0530

    import java.util.Scanner; class vamsi {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String a=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(a);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class vamsi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(a);
+        
+    }
+}
